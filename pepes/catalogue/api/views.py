01734f336d2d9b3aec866881f258a10196f7316c
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-02-27 03:55:52 +0100

    Add route for api

--- pepes/catalogue/api/views.py
+++ pepes/catalogue/api/views.py
@@ -1,8 +1,8 @@
-from rest_framework import generics
-from catalogue import Pepe
+from catalogue.models import Pepe
+from rest_framework import viewsets
 from . import serializers
 
 
-class PepeView(generics.ModelViewSet):
+class PepeView(viewsets.ModelViewSet):
     queryset = Pepe.objects.all()
     serializer_class = serializers.PepeSerializer
--- pepes/pepes/urls.py
+++ pepes/pepes/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 
 from . import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('catalogue.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
