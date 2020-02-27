4fbe49e4e8cb4ae299d4662701efe9b1c9f3c70f
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-02-27 03:22:59 +0100

    Register model in admin site, add media to gitignore
    I don't want to store images in the repository

--- pepes/catalogue/models.py
+++ pepes/catalogue/models.py
@@ -3,5 +3,7 @@ from django.db import models
 
 class Pepe(models.Model):
     description = models.TextField(max_length=200, blank=True)
-    image = models.ImageField()
-    
+    image = models.ImageField(upload_to='media')
+
+    def __str__(self):
+    return self.description[:20]
