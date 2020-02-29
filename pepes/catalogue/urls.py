f4735b68890d8e22024fadb5a473a55fefc54fbb
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-02-29 09:12:53 +0100

    Add index template

--- pepes/catalogue/urls.py
+++ pepes/catalogue/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .api.views import PepeView
+from .views import IndexView
 
 
 router = routers.DefaultRouter()
@@ -8,5 +9,6 @@ router.register('pepes', PepeView)
 
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('api/', include(router.urls))
 ]
