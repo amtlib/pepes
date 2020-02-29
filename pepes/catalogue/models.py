37896997d5c3f68e8f1a556307cb9245402f4790
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-02-29 09:04:42 +0100

    Do not duplicate 'media' in image url

--- pepes/catalogue/models.py
+++ pepes/catalogue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pepe(models.Model):
     description = models.TextField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='media')
+    image = models.ImageField()
 
     def __str__(self):
         return self.description[:20]
