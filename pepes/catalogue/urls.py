6edcbf9a0460e1a241ec8b3c4f3bff48ed8d7b2f
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-03-01 08:27:47 +0100

    Remove catalogue view

--- pepes/catalogue/urls.py
+++ pepes/catalogue/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
 from .api.views import PepeView
-from .views import IndexView
 
 
 router = routers.DefaultRouter()
@@ -9,6 +8,5 @@ router.register('pepes', PepeView)
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
     path('api/', include(router.urls))
 ]
--- pepes/catalogue/views.py
+++ pepes/catalogue/views.py
@@ -1,7 +1,1 @@
 from django.shortcuts import render
-from django.views import View
-
-
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'catalogue/index.html')
